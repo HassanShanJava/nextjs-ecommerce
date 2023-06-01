a29308b9dc27a680774eb05fc03ebe0ae7436fba
HassanShanJava <hassanshan675@gmail.com>
2023-06-02 03:23:26 +0500

    added changes

--- sanity.config.ts
+++ sanity.config.ts
@@ -20,8 +20,6 @@ export default defineConfig({
     deskTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
-    // https://www.sanity.io/docs/the-vision-plugin
-    // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
   ],
 })
